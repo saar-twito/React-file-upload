7f53027688eb314413b81d691ab688b25e7a9682
Saar Twito <saartwito49@gmail.com>
2021-04-28 09:08:45 +0300

    Made it better

--- server.js
+++ server.js
@@ -24,34 +24,34 @@ mongoose.connect('mongodb://127.0.0.1/Test',
 // Upload Endpoint
 app.post('/upload', (req, res) => {
 
-  // Check if user unloaded file
-  if (req.files === null)
-    return res.status(400).send('No file uploaded');
+  try {
+    // Check if user unloaded file
+    if (req.files === null) return res.status(400).send('No file uploaded');
 
-  const file = req.files.file;
+    const file = req.files.file;
+    const format = file.mimetype.split('/')
+    const allowedFormats = ['webp', 'png', 'jpg', 'jpeg', 'tiff']
 
-  const allowedFormats = ['webp', 'png', 'jpg', 'jpeg', 'tiff']
-
-  const format = file.mimetype.split('/')
-  if (!allowedFormats.includes(format[1]))
-    return res.status(400).send('Image has to be (png, jpg, jpeg, tiff)');
+    if (!allowedFormats.includes(format[1])) return res.status(400).send('Image has to be (png, jpg, jpeg, tiff)');
+    // Check the size of the image not ove then 1.5 MB
+    if (file.size >= 1500000) return res.status(400).send('Image\'s size must be equal or smaller than 1.5 MB');
 
 
-  // Check the size of the image not ove then 1.5 MB
-  if (file.size >= 1500000)
-    return res.status(400).send('Image\'s size must be equal or smaller than 1.5 MB');
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, async err => {
 
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, async err => {
+      if (err) return res.status(500).send(err);
 
-    if (err) return res.status(500).send(err);
+      const foodCategory = new FoodCategory({
+        imagesPath: `/uploads/${file.name}`,
+        fileName: file.name
+      });
 
-    const foodCategory = new FoodCategory({
-      imagesPath: `/uploads/${file.name}`,
-      fileName: file.name
+      res.send(await foodCategory.save());
     });
+  } catch (error) {
+    res.send("Couldn't completed the upload process " + error.message);
+  }
 
-    res.send(await foodCategory.save());
-  });
 });
 
 app.listen(5000, () => console.log('Server Started...'));
